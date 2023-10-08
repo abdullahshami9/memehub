2e356785fc8a874d3c41593bc3cd5a566d9795e2
Abdullah Shahmeer <abdullahshahmeer.2001@gmail.com>
2023-10-08 18:52:32 +0500

    blade boiler platecode and login with google/fb is setup working fine but fb login is making trouble i think i should give a bit time as yesterday for google

---
+++ routes/api.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Http\Controllers\LoginController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+// Route::post('auth/{provider}/redirect', [LoginController::class, 'redirect']);
+// Route::post('auth/{provider}/callback', [LoginController::class, 'callback']);
+
+
+Route::post('user/data', [LoginController::class, 'userdata']);
